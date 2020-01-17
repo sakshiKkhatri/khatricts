6a1d5ec68ab18146e04cc86828541c36de4de245
Alchemy <Alchemy@192.168.1.176>
2020-01-17 02:48:06 -0800

    spring

---
+++ spring_demo/src/main/java/comm/mycompany/spring_demo/App.java
@@ -0,0 +1,13 @@
+package comm.mycompany.spring_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
