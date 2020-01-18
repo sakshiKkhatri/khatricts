4435a89110efb2ebc3a34ee94edd8afcc811f968
Alchemy <Alchemy@192.168.1.176>
2020-01-18 04:10:30 -0800

    january 18

---
+++ jan_18/src/main/java/comm/mycompany/jan_18/Service.java
@@ -0,0 +1,25 @@
+package comm.mycompany.jan_18;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Service {
+CustomerDao custdao;
+public Customer createCustomer(String firstname, String lastname, String email, String id)
+{
+	return custdao.createCustomer(firstname, lastname, email, id);
+}
+public List<Customer> displayCustomer()
+{
+		return custdao.displayCustomer();
+}
+public Customer findById(String id)
+{
+	
+	return custdao.findById(id);	
+} 
+}
